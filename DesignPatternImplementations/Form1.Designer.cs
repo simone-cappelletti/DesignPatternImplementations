6e0f443abc2d63150736771e54f459a6dd3cd6c4
scappelletti <scappelletti@20220043-AIT>
2022-11-03 21:52:00 +0100

    Add project files.

---
+++ DesignPatternImplementations/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DesignPatternImplementations
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSrategyStart = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSrategyStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(124, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "STRATEGY";
+            // 
+            // btnSrategyStart
+            // 
+            this.btnSrategyStart.Location = new System.Drawing.Point(6, 22);
+            this.btnSrategyStart.Name = "btnSrategyStart";
+            this.btnSrategyStart.Size = new System.Drawing.Size(75, 23);
+            this.btnSrategyStart.TabIndex = 0;
+            this.btnSrategyStart.Text = "Start";
+            this.btnSrategyStart.UseVisualStyleBackColor = true;
+            this.btnSrategyStart.Click += new System.EventHandler(this.btnSrategyStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 279);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnSrategyStart;
+    }
+}
---
+++ DesignPatternImplementations/Form1.cs
@@ -0,0 +1,15 @@
+namespace DesignPatternImplementations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSrategyStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
